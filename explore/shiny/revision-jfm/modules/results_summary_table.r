630c15f38eb4352ce4857e0cddaa3a6a7d85c0e6
bautheac <o.bautheac@gmail.com>
2024-06-14 15:48:28 +0100

    Add download button to table module

--- explore/shiny/revision-jfm/modules/results_summary_table.r
+++ explore/shiny/revision-jfm/modules/results_summary_table.r
@@ -6,7 +6,11 @@ results_summary_table_UI <- function(id, table_name){
   ns <- NS(id)
   
   tagList(
-    h3(table_name),
+    fluidRow(
+      column(6, align = "left", h3(table_name)),
+      column(6, align = "right", downloadButton(ns('download'), 'Download .csv')
+      )
+    ),
     fluidRow(DT::dataTableOutput(ns("table")), width = "100%", height = "auto", fill = TRUE),
   )
 }
@@ -17,8 +21,18 @@ results_summary_table_Server <- function(id, table) {
     
     ns <- session$ns
     
+    table_reactive <- reactive(table)
+    
     output$table <- DT::renderDataTable(
-      table, filter = 'top', options = list(scrollX = TRUE)
+      table_reactive(), filter = 'top', options = list(scrollX = TRUE)
+    )
+    output$download <- downloadHandler(
+      filename = function() {
+        paste0("results-", Sys.time(), ".csv")
+      },
+      content = function(file) {
+        readr::write_csv(table_reactive(), file)
+      }
     )
   })
 }
