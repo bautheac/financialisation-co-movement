fb14f82fdee92f887657de632482584b945903ba
bautheac <o.bautheac@gmail.com>
2024-08-21 11:23:19 +0100

    Table 1: descriptive statistics (no regimes)

--- explore/scripts/revision-jfm-tables.r
+++ explore/scripts/revision-jfm-tables.r
@@ -13,41 +13,70 @@ factors <- c("market", "CHP", "open interest nearby", "open interest aggregate",
 sector_levels <- c("all", "agriculturals", "energy", "metals")
 subsector_levels <- c("all", "grains", "livestock", "softs", "gas", "petroleum", "base", "precious")
 
-# `US commodities ~ factors from US commodities ####
-path <- paste0(here::here(), "/explore/results/revision-jfm/regressions-factor.rds")
-raw <- readRDS(path)
 
-formatted <- dplyr::filter(raw, factor %in% factors, timespan == "period") %>%
-  dplyr::select(-c(timespan, year, field, type, frequency, sector, subsector)) %>%
+# descriptive stats ####
+stats <- readr::read_rds(
+  here::here("explore", "results", "revision-jfm", "descriptive-statistics-clean.rds")
+)
+
+commodities_market_individuals <- dplyr::filter(stats, analysis == "commodity futures") %>%
+  dplyr::select(results) %>% tidyr::unnest(results) %>%
+  dplyr::filter(analysis == "market variables") %>%
+  dplyr::select(results) %>% tidyr::unnest(results) %>%
+  dplyr::filter(assets == "individual commodities") %>%
+  dplyr::select(results) %>% tidyr::unnest(results) %>%
+  dplyr::filter(
+    type == "returns", period != "year", frequency == "daily", 
+    field == "close price"
+  ) %>% dplyr::select(-c(ticker, type, frequency, field, year)) %>%
+  dplyr::mutate_if(.predicate = is.list, .funs = unlist)
+
+commodities_market_countries <- dplyr::filter(stats, analysis == "commodity futures") %>%
+  dplyr::select(results) %>% tidyr::unnest(results) %>%
+  dplyr::filter(analysis == "market variables") %>%
+  dplyr::select(results) %>% tidyr::unnest(results) %>%
+  dplyr::filter(assets == "countries - sectors - subsectors") %>%
+  dplyr::select(results) %>% tidyr::unnest(results) %>%
+  dplyr::filter(
+    sector == "all", subsector == "all", field == "close price",
+    type == "returns", period != "year", frequency == "daily"
+  ) %>% dplyr::select(-c(type, frequency, field, year)) %>%
+  dplyr::mutate_if(.predicate = is.list, .funs = unlist)
+
+## whole ####
+commodities <- dplyr::filter(commodities_market_individuals, regime == "all") %>%
+  dplyr::select(-c(regime, min, max)) %>% 
   dplyr::mutate(
-    period = factor(period, levels = c("past", "financialization", "crisis", "present")),
-    factor = factor(factor, levels = factors),
-    average = percentize(average)
-  ) %>% dplyr::relocate(regime, .after = leg) %>% 
-  dplyr::arrange(country, factor, leg, period, regime) %>%
-  tidyr::pivot_wider(names_from = "period", values_from = "average")
+    mean = percentize(mean), sd = percentize(sd),
+    mean = purrr::map2_chr(p.value, mean, ~ significance(.x, .y))
+  ) %>% dplyr::select(-p.value) %>%
+  tidyr::pivot_longer(
+    cols = c("mean", "sd"), names_to = "estimate", values_to = "value"
+  ) %>% tidyr::pivot_wider(names_from = "period", values_from = "value") %>%
+  dplyr::arrange(dplyr::desc(country), sector, subsector, commodity) %>%
+  dplyr::select(-c(country, sector, subsector)) %>%
+  dplyr::rename(asset = commodity)
 
-path <- paste0(here::here(), "/explore/tables/revision-jfm/regressions-factors.csv")
-readr::write_csv(formatted, path)
-
-# `all commodities ~ factors from US commodities ####
-path <- paste0(here::here(), "/explore/tables/revision-jfm/regressions-factors.rds")
-raw <- readRDS(path)
-
-formatted <- dplyr::filter(raw, factor %in% factors, timespan == "period") %>%
-  dplyr::select(-c(timespan, year, field, type, frequency)) %>%
+countries <- dplyr::filter(commodities_market_countries, regime == "all") %>%
+  dplyr::select(-c(regime, min, max, sector, subsector)) %>% 
+  dplyr::mutate(asset = paste(country, "commodities", sep = " ")) %>%
+  dplyr::select(-country) %>% 
   dplyr::mutate(
-    country = factor(country, levels = c("all", "US", "GB")),
-    sector = factor(sector, levels = c("all", "agriculturals", "energy", "metals")),
-    subsector = factor(subsector, levels = c("all", "base", "gas", "grains", "livestock", "petroleum", "precious", "softs")),
-    period = factor(period, levels = c("past", "financialization", "crisis", "present")),
-    factor = factor(factor, levels = factors),
-    average = percentize(average)
-  ) %>% dplyr::relocate(regime, .after = leg) %>% 
-  dplyr::arrange(country, sector, subsector, factor, leg, period, regime) %>%
-  tidyr::pivot_wider(names_from = "period", values_from = "average") %>%
-  dplyr::filter(factor != "open interest aggregate")
+    mean = percentize(mean), sd = percentize(sd),
+    mean = purrr::map2_chr(p.value, mean, ~ significance(.x, .y))
+  ) %>% dplyr::select(-p.value) %>%
+  tidyr::pivot_longer(
+    cols = c("mean", "sd"), names_to = "estimate", values_to = "value"
+  ) %>% tidyr::pivot_wider(names_from = "period", values_from = "value")
 
-path <- paste0(here::here(), "/explore/tables/revision-jfm/regressions-factors.csv")
+whole <- dplyr::bind_rows(commodities, countries)
+
+
+tables <- tibble::tibble(
+  analysis = "descriptive statistics (no regimes)", results = list(whole)
+)
+
+
+path <- here::here("explore", "tables", "revision-jfm", "regressions-factors.csv")
 readr::write_csv(formatted, path)
 
